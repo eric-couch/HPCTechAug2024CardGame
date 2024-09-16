4ae77ad8156315644a9b34b0a67ec2ea1d72b4fe
Eric Couch <ericcouch@yahoo.com>
2024-09-16 13:48:43 -0500

    Add project files.

---
+++ HPCTechAug2024CardGame/Deck.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HPCTechAug2024CardGame;
+
+public class Deck
+{
+    // list of cards
+    public List<card> Cards { get; set; }
+    public int totalCards { get; set; }
+    public int cardsRemaining { get; set; }
+
+    public Deck()
+    {
+        // we need to create the cards in the deck
+        List<string> suits = new List<string> { "♦", "♣", "♠", "♥" };
+        List<string> ranks = new List<string> { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+        List<int> values = new List<int> { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+
+        // Iiterable
+        foreach (string suit in suits)
+        {
+            foreach (string rank in ranks)
+            {
+                Cards.Add(new card(suit, rank, values[rank.IndexOf(rank)]));
+                cardsRemaining++;
+            }
+        }
+        totalCards = cardsRemaining;
+    }
+
+
+
+    // do we really need this?
+    public void Shuffle()
+    {
+        
+    }
+}
---
+++ HPCTechAug2024CardGame/card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HPCTechAug2024CardGame;
+
+public class card
+{
+    public string suit { get; set; }
+    public string rank { get; set; }
+    public int value { get; set; }
+
+    public card(string suit, string rank, int value)
+    {
+        this.suit = suit;
+        this.rank = rank;
+        this.value = value;
+    }
+
+
+}
